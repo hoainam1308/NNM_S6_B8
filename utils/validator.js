c7d31df96f8fa045b732e7736c685b2792c73f97
hoainam <hoainam1511ytn@gmail.com>
2025-03-29 10:05:17 +0700

    firstcm

---
+++ utils/validator.js
@@ -0,0 +1,90 @@
+let { body, validationResult } = require('express-validator')
+let constants = require('./constants')
+let util = require('util')
+
+let options = {
+    password:{
+        minLength: 8,
+        minLowercase: 1,
+        minUppercase: 1,
+        minNumbers: 1,
+        minSymbols: 1
+    },
+    username:{
+        minLength: 6
+    }
+}
+
+module.exports = {
+    validate: function (req, res, next) {
+        let errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            CreateErrorResponse(res, 400, errors.array())
+        } else {
+            next();
+        }
+    },
+    SignUpValidator: [
+        body("username").isLength(options.username).withMessage(util.format(constants.VALIDATOR_ERROR_USERNAME,options.username.minLength)),
+        body("password").isStrongPassword(options.password).withMessage(util.format(constants.VALIDATOR_ERROR_PASSWORD,
+            options.password.minLength,
+            options.password.minLowercase,
+            options.password.minUppercase,
+            options.password.minNumbers,
+            options.password.minSymbols)),
+        body("email").isEmail().withMessage(constants.VALIDATOR_ERROR_EMAIL)
+    ],
+    LoginValidator: [
+        body("username").isLength(options.username).withMessage("username hoac password sai"),
+        body("password").isStrongPassword(options.password).withMessage("username hoac password sai")
+    ],
+    UpdateUserValidator: [
+        body("username").isLength(options.username).withMessage(util.format(constants.VALIDATOR_ERROR_USERNAME,options.username.minLength)),
+        body("email").isEmail().withMessage(constants.VALIDATOR_ERROR_EMAIL)
+    ],
+    ChangePasswordValidator: [
+        body("oldpassword").isStrongPassword(options.password).withMessage(util.format(constants.VALIDATOR_ERROR_PASSWORD,
+            options.password.minLength,
+            options.password.minLowercase,
+            options.password.minUppercase,
+            options.password.minNumbers,
+            options.password.minSymbols)),
+        body("newpassword").isStrongPassword(options.password).withMessage(util.format(constants.VALIDATOR_ERROR_PASSWORD,
+            options.password.minLength,
+            options.password.minLowercase,
+            options.password.minUppercase,
+            options.password.minNumbers,
+            options.password.minSymbols))
+    ],
+    ForgotPasswordValidator: [
+        body("email").isEmail().withMessage(constants.VALIDATOR_ERROR_EMAIL)
+    ],
+    ResetPasswordValidator: [
+        body("password").isStrongPassword(options.password).withMessage(util.format(constants.VALIDATOR_ERROR_PASSWORD,
+            options.password.minLength,
+            options.password.minLowercase,
+            options.password.minUppercase,
+            options.password.minNumbers,
+            options.password.minSymbols))
+    ],
+    CreateUserValidator: [
+        body("username").isLength(options.username).withMessage(util.format(constants.VALIDATOR_ERROR_USERNAME,options.username.minLength)),
+        body("password").isStrongPassword(options.password).withMessage(util.format(constants.VALIDATOR_ERROR_PASSWORD,
+            options.password.minLength,
+            options.password.minLowercase,
+            options.password.minUppercase,
+            options.password.minNumbers,
+            options.password.minSymbols)),
+        body("email").isEmail().withMessage(constants.VALIDATOR_ERROR_EMAIL),
+        body("role").isMongoId().withMessage("role khong hop le")
+    ],
+    UpdateUserValidator: [
+        body("username").isLength(options.username).withMessage(util.format(constants.VALIDATOR_ERROR_USERNAME,options.username.minLength)),
+        body("email").isEmail().withMessage(constants.VALIDATOR_ERROR_EMAIL),
+        body("role").isMongoId().withMessage("role khong hop le")
+        body("fullname").optional().isString().withMessage("fullname phai la chuoi"),
+        body("avatarUrl").optional().isString().withMessage("avatarUrl phai la chuoi"),
+        body("status").optional().isBoolean().withMessage("status phai la boolean")
+    ],
+}
+// multer
